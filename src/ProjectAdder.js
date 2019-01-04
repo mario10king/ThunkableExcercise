fbd3d825b6e2f22fa65deb9dbd3861bc1ec9631f
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2019-01-04 11:23:09 -0800

    Add style to Project Adder

--- src/ProjectAdder.js
+++ src/ProjectAdder.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
+import PlusSign from './assets/Plus\ Sign.svg'
+import './ProjectAdder.css'
 
 class ProjectAdder extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      Style: {},
+    }
+
+    this.handleHover = this.handleHover.bind(this)
+  }
+
+  handleHover(hovering) {
+    var hoveringStyle = {background: '#3D3A4F', boxShadow: '0px 6px 6px rgba(0, 0, 0, 0.25)'};
+    var style = hovering ? hoveringStyle : {};
+    this.setState({Style: style});
+  }
+
   render() {
     return ( 
-        <button onClick={this.props.handleClick} style={{position: 'absolute'}}>Add Project</button> 
+      <div onClick={this.props.handleClick} style={this.state.Style} onMouseEnter={() => this.handleHover(true)} onMouseLeave={() => this.handleHover(false)} className="project-adder">
+        <img id='plus-sign' src={PlusSign}/>
+      </div>
     )
   }
 }
